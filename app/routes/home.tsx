8ac2e8134c2903f875e2551bed1da94385c22046
Chitresh-code <gychitresh1290@gmail.com>
2025-07-19 23:41:29 +0530

    work on homepage 19-Jul-11:41

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,28 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    { name: "description", content: "Smart Feedback on your Resume for your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review Your Submissions and get AI Powered Feedback</h2>
+      </div>
+    </section>
+
+    {resumes.map((resume) => (
+      <ResumeCard key={resume.id} resume={resume} />
+    ))}
+  </main>
 }
