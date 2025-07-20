633b6151875d585437822a994984b9c49f4a112c
Chitresh-code <gychitresh1290@gmail.com>
2025-07-20 21:54:18 +0530

    added cloud storage, pdf2img conversion, AI resume feedback

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,14 +1,95 @@
+import { prepareInstructions } from '../../constants';
 import { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
+import { convertPdfToImage } from '~/lib/pdf2img';
+import { usePuterStore } from '~/lib/puter';
+import { generateUUID } from '~/lib/utils';
 
 const Upload = () => {
+    const { auth, isLoading, fs, ai, kv } = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null>(null);
 
     const handleFileSelect = (file: File | null) => {
         setFile(file);
+    }
+
+    const handleAnalyze = async ({ companyName, jobTitle, jobDescription, file }: { companyName: string, jobTitle: string, jobDescription: string, file: File }) => {
+        setIsProcessing(true);
+        setStatusText('Uploading your resume...');
+
+        const uploadedFile = await fs.upload([file]);
+
+        if (!uploadedFile) {
+            setIsProcessing(false);
+            setStatusText('Failed to upload the file. Please try again.');
+            return;
+        }
+
+        setStatusText('Converting your resume to ATS friendly format...');
+        const imageFile = await convertPdfToImage(file);
+
+        if (!imageFile.file) {
+            setIsProcessing(false);
+            setStatusText('Failed to convert the file. Please try again.');
+            return;
+        }
+
+        setStatusText('Working the magic...');
+        const uploadedImage = await fs.upload([imageFile.file]);
+        if (!uploadedImage) {
+            setIsProcessing(false);
+            setStatusText('Failed to process the resume. Please try again.');
+            return;
+        }
+
+        setStatusText('Preparing the data for analysis...');
+        const uuid = generateUUID();
+
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName,
+            jobTitle,
+            jobDescription,
+            feedback: '',
+        }
+
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Analyzing your resume...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({ jobTitle, jobDescription }),
+        )
+
+        if (!feedback) {
+            setIsProcessing(false);
+            setStatusText('Failed to analyze the resume. Please try again.');
+            return;
+        }
+
+        setStatusText('Processing the feedback...');
+        const feedbackText = typeof feedback.message.content === 'string'
+            ? feedback.message.content
+            : feedback.message.content[0].text;
+
+        data.feedback = JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+        setStatusText('Done! Redirecting you to the results page...');
+        console.log('Result:', data);
+        // setTimeout(() => {
+        //     setIsProcessing(false);
+        //     navigate(`/results/${uuid}`);
+        // }, 2000);
+
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -23,12 +104,7 @@ const Upload = () => {
 
         if(!file) return;
 
-        console.log('Submitting form with:', {
-            companyName,
-            jobTitle,
-            jobDescription,
-            fileName: file.name,
-        });
+        handleAnalyze({ companyName, jobTitle, jobDescription, file });
     }
 
     return ( 
